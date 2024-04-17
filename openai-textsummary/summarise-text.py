f9349cd2761299fdfaf4e5c23a8173f27dcbddd8
soderholmd <101971880+soderholmd@users.noreply.github.com>
2024-04-17 09:29:39 +0100

    Update summarise-text.py

--- openai-textsummary/summarise-text.py
+++ openai-textsummary/summarise-text.py
@@ -1,15 +1,17 @@
 '''
-Azure OpenAI Text Summariser
+Azure OpenAI text summariser
 Daniel Soderholm 2024
 
 The code is a Python script that uses the Azure OpenAI API to generate text summaries. 
-It takes command line arguments to specify the input text, either from a file or a URL, and allows the user to interactively enter a prompt if no file or URL is provided. 
-The script sends a request to the Azure OpenAI model, passing the input text as a message. The generated summary is then printed to the console. 
+It takes command line arguments to specify the input text, either from a file (TXT/DOCX/PDF) or a URL, and allows the user to interactively enter a prompt if no file or URL is provided. 
+The script sends a request to the Azure OpenAI model, passing the input text as a message. 
+The generated summary is then printed to the console. 
 If the --stats flag is included, the script also prints statistics about the token usage and cost of the transaction.
+
 URLs might not work because they generate a lot of text and the API has a limit on the number of tokens that can be processed in a single request. 
+
 The script uses the AzureOpenAI class from the OpenAI module to interact with the Azure OpenAI API. 
-It defaults to GPT-3.5 (or whatever you save as AZURE_OAI_DEPLOYMENT), but has a flag to use GPT-4 instead. 
-GPT-4 will provide better results but runs about 10x slower than GPT-3.5, and costs about 50% more.
+
 The script also reads the Azure OpenAI endpoint and key from the .env file using the dotenv package.
 '''
 
@@ -17,7 +19,9 @@ import os
 import argparse
 import requests
 import time
+import PyPDF2
 from dotenv import load_dotenv
+from docx import Document
 
 # Add Azure OpenAI package
 from openai import AzureOpenAI
@@ -58,8 +62,16 @@ def main():
 
         # Get input text
         if args.file:
-            with open(args.file, 'r') as file:
-                input_text = file.read()
+            if args.file.endswith('.docx'):
+                doc = Document(args.file)
+                input_text = ' '.join(paragraph.text for paragraph in doc.paragraphs)
+            elif args.file.endswith('.pdf'):
+                with open(args.file, 'rb') as file:
+                    reader = PyPDF2.PdfReader(file)
+                    input_text = ' '.join(reader.pages[i].extract_text() for i in range(len(reader.pages)))
+            else:
+                with open(args.file, 'r') as file:
+                    input_text = file.read()
         elif args.url:
             response = requests.get(args.url)
             response.raise_for_status()  # Raise an exception if the GET request failed
